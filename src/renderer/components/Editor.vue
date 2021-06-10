82eedf37ef9b2d5168a1813ce160370bfae0a433
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-06-10 10:26:49 -0500

    Toggle editor with markdown preview: - setup marked.js to render markdown:q - setup tailwind typography UI

--- src/renderer/components/Editor.vue
+++ src/renderer/components/Editor.vue
@@ -1,10 +1,36 @@
 <template>
   <!-- INSIGHT: Value is bound to CodeMirror so this won't update markdown. -->
-  <textarea id="editor" :value="markdown"></textarea>
+  <div
+    class="
+      h-10
+      text-xl
+      font-black
+      border
+      flex
+      justify-between
+      place-items-center
+      items-center
+    "
+  >
+    <div>{{ openFileName }}</div>
+    <button
+      @click="showEditor = !showEditor"
+      class="px-4 text-2xl h-full hover:bg-sidebar-bg-hover focus:outline-none"
+    >
+      📝
+    </button>
+  </div>
+  <div v-if="showEditor">
+    <textarea id="editor" :value="markdown"></textarea>
+  </div>
+  <div v-else>
+    <div v-html="compiledMarkdown" class="prose dark:prose-light"></div>
+  </div>
 </template>
 
 <script>
 import CodeMirror from 'codemirror';
+import * as marked from 'marked';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/dracula.css';
 import 'codemirror/theme/gruvbox-dark.css';
@@ -16,38 +42,14 @@ export default {
   data() {
     return {
       markdown: '',
+      compiledMarkdown: '',
+      showEditor: true,
       switchingFiles: false,
     };
   },
   mounted() {
-    this.cm = CodeMirror.fromTextArea(document.getElementById('editor'), {
-      mode: 'gfm',
-      theme: themes[this.theme].codeMirror,
-      // fullScreen: true,
-      // lineNumbers: true,
-      viewportMargin: Infinity,
-    });
-    this.cm.setSize('full', '100%');
-
-    this.cm.on('change', (cm) => {
-      // INSIGHT: Don't update CodeMirror when switching files.
-      // INSIGHT: ESPECIALLY DON'T WRITE FILE!!!
-      if (!this.switchingFiles) {
-        this.markdown = cm.getValue();
-        this.writeFile({
-          path: this.openFilePath,
-          markdown: this.markdown,
-        });
-      }
-    });
-
-    // INSIGHT: handle reply from the backend
-    window.ipc.on('READ_FILE', (markdown) => {
-      this.cm.setValue(markdown);
-      this.switchingFiles = false;
-    });
-
-    this.readFile({ path: this.openFilePath });
+    const firstRun = true;
+    this.mountCodeMirror(firstRun);
   },
   methods: {
     writeFile({ path, markdown }) {
@@ -71,15 +73,54 @@ export default {
     setLineNumbers(lineNumbers) {
       this.cm.setOption('lineNumbers', lineNumbers);
     },
+    async mountCodeMirror(firstRun = false) {
+      setTimeout(() => {
+        if (this.showEditor) {
+          this.cm = CodeMirror.fromTextArea(document.getElementById('editor'), {
+            mode: 'gfm',
+            theme: themes[this.theme].codeMirror,
+            // fullScreen: true,
+            // lineNumbers: true,
+            viewportMargin: Infinity,
+          });
+        }
+
+        this.cm.setSize('full', '100%');
+
+        this.cm.on('change', (cm) => {
+          // INSIGHT: Don't update CodeMirror when switching files.
+          // INSIGHT: ESPECIALLY DON'T WRITE FILE!!!
+          if (!this.switchingFiles) {
+            this.markdown = cm.getValue();
+            this.compiledMarkdown = marked(this.markdown, { sanitize: true });
+            this.writeFile({
+              path: this.openFilePath,
+              markdown: this.markdown,
+            });
+          }
+        });
+
+        if (firstRun) {
+          // INSIGHT: handle reply from the backend
+          window.ipc.on('READ_FILE', (markdown) => {
+            this.cm.setValue(markdown);
+            this.switchingFiles = false;
+          });
+
+          this.readFile({ path: this.openFilePath });
+        }
+      }, 10);
+    },
   },
   computed: {
-    ...mapGetters(['openFilePath', 'theme', 'lineNumbers']),
+    ...mapGetters(['openFilePath', 'theme', 'lineNumbers', 'openFileName']),
   },
   watch: {
     // INSIGHT: this runs openFile if 'store.openFilePath' changes
     theme: 'setTheme',
     openFilePath: 'setOpenFile',
     lineNumbers: 'setLineNumbers',
+    showEditor: 'mountCodeMirror',
   },
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,97 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: {
     content: ['./public/**/*.html', './src/**/*.vue'],
   },
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      // INSIGHT: Displaying transparent screens.
-      // 'sidebar-bg': ({ opacityValue }) => {
-      //   if (opacityValue !== undefined) {
-      //     return `rgba(var(--sidebar-bg), ${opacityValue})`;
-      //   }
-      //   return `rgb(var(--sidebar-bg))`;
-      // },
-      'sidebar-bg': 'var(--sidebar-bg)',
-      'sidebar-bg-hover': 'var(--sidebar-bg-hover)',
-      'item-active': 'var(--item-active)',
-      'item-text-active': 'var(--item-text-active)',
-      'text-normal': 'var(--text-normal)',
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+        // INSIGHT: Displaying transparent screens.
+        // 'sidebar-bg': ({ opacityValue }) => {
+        //   if (opacityValue !== undefined) {
+        //     return `rgba(var(--sidebar-bg), ${opacityValue})`;
+        //   }
+        //   return `rgb(var(--sidebar-bg))`;
+        // },
+        'sidebar-bg': 'var(--sidebar-bg)',
+        'sidebar-bg-hover': 'var(--sidebar-bg-hover)',
+        'item-active': 'var(--item-active)',
+        'item-text-active': 'var(--item-text-active)',
+        'text-normal': 'var(--text-normal)',
+      },
+      typography: (theme) => ({
+        light: {
+          css: [
+            {
+              color: theme('colors.gray.400'),
+              '[class~="lead"]': {
+                color: theme('colors.gray.300'),
+              },
+              a: {
+                color: theme('colors.white'),
+              },
+              strong: {
+                color: theme('colors.white'),
+              },
+              'ol > li::before': {
+                color: theme('colors.gray.400'),
+              },
+              'ul > li::before': {
+                backgroundColor: theme('colors.gray.600'),
+              },
+              hr: {
+                borderColor: theme('colors.gray.200'),
+              },
+              blockquote: {
+                color: theme('colors.gray.200'),
+                borderLeftColor: theme('colors.gray.600'),
+              },
+              h1: {
+                color: theme('colors.white'),
+              },
+              h2: {
+                color: theme('colors.white'),
+              },
+              h3: {
+                color: theme('colors.white'),
+              },
+              h4: {
+                color: theme('colors.white'),
+              },
+              'figure figcaption': {
+                color: theme('colors.gray.400'),
+              },
+              code: {
+                color: theme('colors.white'),
+              },
+              'a code': {
+                color: theme('colors.white'),
+              },
+              pre: {
+                color: theme('colors.gray.200'),
+                backgroundColor: theme('colors.gray.800'),
+              },
+              thead: {
+                color: theme('colors.white'),
+                borderBottomColor: theme('colors.gray.400'),
+              },
+              'tbody tr': {
+                borderBottomColor: theme('colors.gray.600'),
+              },
+            },
+          ],
+        },
+      }),
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      typography: ['dark'],
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
